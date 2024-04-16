e6f1591a9d1125a4a1b518944b3251f16a08568b
EdaAzizi <101657000+EdaAzizi@users.noreply.github.com>
2024-04-16 18:26:53 +0200

    Setting the environment

--- API/obj/Debug/net7.0/server.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1690fd76847aafb3b00d89c04e069f16b979e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
