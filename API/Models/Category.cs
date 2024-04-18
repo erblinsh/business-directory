744119f5bc9c48ce47f9e62bf1838499757c8015
EdaAzizi <101657000+EdaAzizi@users.noreply.github.com>
2024-04-18 23:44:20 +0200

    Category Model | US-02

--- API/Models/Category.cs
+++ API/Models/Category.cs
@@ -1,7 +1,20 @@
 ﻿
+using server.models;
+using System.ComponentModel.DataAnnotations;
+
 namespace server.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public List<Business> Businesses { get; set; }
     }
 }
--- API/obj/Debug/net7.0/server.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdf332f991f093ae461742eb587308f46201b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c549cff3677522ce3e595270783f2a858040876d")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
