8fc8632cf731a719f8eb7a40434c0e03612e5b5d
aidahajdarmataj <aida.p.hajdarmataj@gmail.com>
2024-04-19 03:13:06 +0200

    Review Model | US-03

--- API/Models/Review.cs
+++ API/Models/Review.cs
@@ -2,5 +2,25 @@
 {
     public class Review
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int BusinessId { get; set; }
+
+        [ForeignKeyKey("UserId")]
+        public User User { get; set; }
+
+        [ForeignKeyKey("BusinessId")]
+        public Business Business { get; set; }
     }
 }
--- API/obj/Debug/net7.0/server.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81817ebf42e03124846aef21c60434032713283f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
