ce45414ac57970653944446dcb25cb5bd0cdaad0
EdaAzizi <101657000+EdaAzizi@users.noreply.github.com>
2024-04-19 23:09:00 +0200

    Category DTOs | US-02

---
+++ API/DTOs/Category/CategoryDto.cs
@@ -0,0 +1,16 @@
+
+using server.models;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.DTOs.Category
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ API/DTOs/Category/CreateCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.DTOs.Category
+{
+    public class CreateCategoryDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50, ErrorMessage = "Name cannot have more than 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [MaxLength(255, ErrorMessage = "Description cannot have more than 255 characters")]
+        public string Description { get; set; }
+    }
+}
