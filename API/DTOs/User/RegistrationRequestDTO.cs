7c40e1902f582ed174dc74d2b34dd4a56df1bb23
Lis <malokusecond@gmail.com>
2024-04-20 03:35:36 +0200

    User DTOs | US-04

--- API/DTOs/User/RegistrationRequestDTO.cs
+++ API/DTOs/User/RegistrationRequestDTO.cs
@@ -1,13 +1,10 @@
-﻿namespace server.Models
+﻿namespace business_directory.Models.Dto
 {
-    public class LocalUser
+    public class RegistrationRequestDTO
     {
-        public int Id { get; set; }
-
         public string UserName { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
-
     }
 }
