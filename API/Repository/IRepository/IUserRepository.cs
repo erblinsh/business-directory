31bf297ab2890d77e1f971b66dca2078004385a8
Lis <malokusecond@gmail.com>
2024-04-21 22:51:13 +0200

    Repository and IRepository For User | US-04

---
+++ API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using server.DTO.UserAccess;
+
+namespace server.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO);
+    }
+}
---
+++ API/Repository/UserRepository.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using server.Data;
+using server.DTO.UserAccess;
+using server.Models;
+using server.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace server.Repository.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+        private readonly string _secretKey;
+
+        public UserRepository(ApplicationDbContext db, IMapper mapper, IConfiguration configuration,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _mapper = mapper;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(x => x.UserName == username);
+            return user == null;
+        }
+
+        public async Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registrationRequestDTO.UserName,
+                Email = registrationRequestDTO.UserName,
+                NormalizedEmail = registrationRequestDTO.UserName.ToUpper(),
+                Name = registrationRequestDTO.Name
+            };
+
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDTO.Password);
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                        await _roleManager.CreateAsync(new IdentityRole("business"));
+                    }
+                    await _userManager.AddToRoleAsync(user, "admin");
+                    var userToReturn = _db.ApplicationUsers
+                        .FirstOrDefault(u => u.UserName == registrationRequestDTO.UserName);
+                    return _mapper.Map<UserDTO>(userToReturn);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return new UserDTO();
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = _db.ApplicationUsers
+                .FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower());
+
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDTO.Password);
+
+            if (user == null || isValid == false)
+            {
+                return new LoginResponseDTO()
+                {
+                    Token = "",
+                    User = null
+                };
+            }
+
+            //if user was found generate JWT Token
+            var roles = await _userManager.GetRolesAsync(user);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = _mapper.Map<UserDTO>(user),
+                Role = roles.FirstOrDefault(),
+            };
+            return loginResponseDTO;
+        }
+    }
+}
+
