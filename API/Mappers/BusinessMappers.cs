49322996a12f5ac57470d222d48fb660b4863161
Bini <erblinshabani.life@gmail.com>
2024-04-21 21:45:18 +0200

    Business Mapper | US-01

---
+++ API/Mappers/BusinessMappers.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using server.DTOs.Business;
+using server.models;
+
+namespace server.Mappers
+{
+    public class BusinessMappers : Profile
+    {
+        public BusinessMappers()
+        {
+            CreateMap<Business, BusinessDto>()
+                .ForMember(dest => dest.Category,   // Category that the business belong to
+                            opt => opt.MapFrom(src => src.Category))
+                .ForMember(dest => dest.Reviews,    // Reviews that belong to that business
+                            opt => opt.MapFrom(src => src.Reviews)); 
+
+            CreateMap<BusinessDto, Business>()
+                .ForMember(dest => dest.Category,   // Category that the business belong to
+                            opt => opt.MapFrom(src => src.Category))
+                .ForMember(dest => dest.Reviews,    // Reviews that belong to that business
+                            opt => opt.MapFrom(src => src.Reviews)); 
+
+            CreateMap<CreateBusinessDto, Business>();
+            CreateMap<Business, CreateBusinessDto>();
+
+            CreateMap<UpdateBusinessDto, Business>();
+            CreateMap<Business, UpdateBusinessDto>();
+        }
+    }
+}
--- API/obj/Debug/net7.0/server.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada9d34e7a0d125ffa5339d08e83d5179b4d33d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c40e1902f582ed174dc74d2b34dd4a56df1bb23")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
