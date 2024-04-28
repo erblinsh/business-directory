218029dfbc38e7ee0e5982c0e687ce8a25bb58ab
EdaAzizi <101657000+EdaAzizi@users.noreply.github.com>
2024-04-28 21:50:36 +0200

    Business Details Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './Navbar/Navbar';
+import { Home } from './pages/Home/Home';
+import { Business } from './pages/Business/Business';
+import { BusinessDetails } from './pages/BusinessDetails/BusinessDetails';
 
 function App() {
   return (
     <div className="App">
+       <Router>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/business" element={<Business />}/>
+            <Route path='/business/:id' element={<BusinessDetails />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
---
+++ client/src/pages/BusinessDetails/BusinessDetails.jsx
@@ -0,0 +1,92 @@
+import { useParams } from "react-router-dom"
+import { useGetBusinessByIdQuery } from "../../redux/api/businessApi";
+import { Card, Container, ListGroup, ListGroupItem, Row, Col, Form, Button } from 'react-bootstrap';
+import './businessDetails.css';
+import { useState } from "react";
+
+export const BusinessDetails = () => {
+  const { id } = useParams();
+
+  const { data: {result: business} = {}} = useGetBusinessByIdQuery(id);
+
+  const [reviewText, setReviewText] = useState("");
+  const [reviews, setReviews] = useState(business?.reviews || []);
+
+  const handleReviewSubmit = () => {
+    if (reviewText.trim() !== "") {
+      const newReview = { text: reviewText };
+      setReviews([...reviews, newReview]);
+      setReviewText(""); 
+    }
+  };
+
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col md={6}>
+          <Card.Img className='business-card-image' variant="top" src={business?.imageUrl} />
+        </Col>
+        <Col md={6}>
+          <Card className='business-details-card'> 
+            <Card.Body>
+              <Card.Title>{business?.name}</Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush business-list-group">
+              <ListGroupItem>
+                <Row>
+                  <Col>Address:</Col>
+                  <Col>{business?.address}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Website:</Col>
+                  <Col><a href={business?.websiteUrl} >{business?.websiteUrl}</a></Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Phone:</Col>
+                  <Col>{business?.phoneNumber}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Email:</Col>
+                  <Col>{business?.email}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Ratings:</Col>
+                  <Col>{reviews?.rating}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>{business?.description}</Col>
+                </Row>
+              </ListGroupItem>
+            </ListGroup>
+         </Card>
+         <br />
+         <ListGroupItem>
+            <Form>
+              <Form.Group controlId="reviewText">
+                <Form.Label>Write a Review:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={6}
+                  value={reviewText}
+                  onChange={(e) => setReviewText(e.target.value)}
+                />
+              </Form.Group>
+              <br/>
+              <Button className="button" onClick={handleReviewSubmit}>Submit</Button>
+            </Form>
+          </ListGroupItem>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
